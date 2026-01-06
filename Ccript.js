afb4206c2250e33b22ffe2570d14d4a3ce9ccba3
lynkan581-lgtm <lynkan581@gmail.com>
2026-01-06 16:41:59 +0700

    Create Ccript.js

---
+++ Ccript.js
@@ -0,0 +1,7 @@
+document.body.style.transitionecho "# Moka-love-" >> README.md 
+git init 
+git add README.md 
+git commit -m "first commit" 
+git branch -M main 
+git remote add origin https://github.com/lynkan581-lgtm/Moka-love-.git
+ git push -u origin main
